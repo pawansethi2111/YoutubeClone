a272c934629a019e99b2100a2bf62090c1c43314
Pawan <pawansethi2111@gmail.com>
2025-05-10 15:31:30 +0530

    hello

--- src/data.js
+++ src/data.js
@@ -1,4 +1,5 @@
-export const API_KEY = 'AIzaSyCoZoONxB6_2bqALihj_SLuexTc6sSDiVc';
+// export const API_KEY = 'AIzaSyCoZoONxB6_2bqALihj_SLuexTc6sSDiVc';
+export const API_KEY = import.meta.env.VITE_API_KEY;
 
 export const value_converter = (value)=>{
         if(value>=1000000){
